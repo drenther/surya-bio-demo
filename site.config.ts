fb91aebee0ebfcd85eb0e8000f050174a6693edf
drenther <s.pathak5995@gmail.com>
2022-10-19 10:45:27 +0530

    update config and github link

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Suryadeep Pal',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Suryadeep Pal - Software Engineer',
 
   // social usernames (optional)
   twitter: 'techpool',
